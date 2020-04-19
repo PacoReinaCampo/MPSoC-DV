ba8a1405f61752f97418d9fb0957771c083a1146
PacoReinaCampo <frareicam@gmail.com>
2020-04-19 18:38:13 +0200

    new commit

---
+++ MPSoC-OR/software/build.sh
@@ -0,0 +1,14 @@
+export PATH=/opt/toolchains/or1k-elf-multicore/bin:${PATH}
+
+if [ -n "$BASH_SOURCE" ]; then
+   OPTIMSOC="`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd `"
+elif [ -n "$ZSH_NAME" ]; then
+   OPTIMSOC="`dirname $0`"
+else
+   OPTIMSOC="`pwd`"
+fi
+
+export OPTIMSOC
+
+export PKG_CONFIG_PATH=$OPTIMSOC/baremetal/baremetal-libs/sw/share/pkgconfig:$PKG_CONFIG_PATH
+export PATH=$OPTIMSOC/baremetal/host/bin:$PATH
