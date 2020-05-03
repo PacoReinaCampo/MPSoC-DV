9b6d7eb2bf9d8c9dd15bb69c8ee87eb932d61fec
PacoReinaCampo <frareicam@gmail.com>
2020-05-03 05:25:19 +0200

    new commit

--- MPSoC-MSP/software/os/gzll-kernel/README.md
+++
@@ -1,4 +0,0 @@
-# gzll Kernel
-
-This is the gzll kernel, for more info we refer you to the website
-http://www.gzll.org
--- MPSoC-MSP/software/os/gzll-kernel/src/memcpy_userspace.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MEMCPY_USERSPACE_H__
-#define __MEMCPY_USERSPACE_H__
-
-#include <stddef.h>
-
-extern void gzll_memcpy_from_userspace(void* dest, void* src, size_t size);
-extern void gzll_memcpy_to_userspace(void* dest, void* src, size_t size);
-
-#endif
