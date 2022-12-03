00c817436c986ce1765137f86879d6a4fc848e85
PacoReinaCampo <frareicam@gmail.com>
2022-12-04 00:33:27 +0100

    Working in MPSoC-DV 🚀

---
+++ quality/ada/library/arithmetic/scalar/ntm_scalar_arithmetic.adb
@@ -0,0 +1,80 @@
+-----------------------------------------------------------------------------------
+--                                            __ _      _     _                  --
+--                                           / _(_)    | |   | |                 --
+--                __ _ _   _  ___  ___ _ __ | |_ _  ___| | __| |                 --
+--               / _` | | | |/ _ \/ _ \ '_ \|  _| |/ _ \ |/ _` |                 --
+--              | (_| | |_| |  __/  __/ | | | | | |  __/ | (_| |                 --
+--               \__, |\__,_|\___|\___|_| |_|_| |_|\___|_|\__,_|                 --
+--                  | |                                                          --
+--                  |_|                                                          --
+--                                                                               --
+--                                                                               --
+--              Peripheral-NTM for MPSoC                                         --
+--              Neural Turing Machine for MPSoC                                  --
+--                                                                               --
+-----------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------
+--                                                                               --
+-- Copyright (c) 2020-2024 by the author(s)                                      --
+--                                                                               --
+-- Permission is hereby granted, free of charge, to any person obtaining a copy  --
+-- of this software and associated documentation files (the "Software"), to deal --
+-- in the Software without restriction, including without limitation the rights  --
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     --
+-- copies of the Software, and to permit persons to whom the Software is         --
+-- furnished to do so, subject to the following conditions:                      --
+--                                                                               --
+-- The above copyright notice and this permission notice shall be included in    --
+-- all copies or substantial portions of the Software.                           --
+--                                                                               --
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    --
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      --
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   --
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        --
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, --
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     --
+-- THE SOFTWARE.                                                                 --
+--                                                                               --
+-- ============================================================================= --
+-- Author(s):                                                                    --
+--   Paco Reina Campo <pacoreinacampo@queenfield.tech>                           --
+--                                                                               --
+-----------------------------------------------------------------------------------
+
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+package body ntm_scalar_arithmetic is
+
+  procedure ntm_scalar_adder (
+    data_a_in : in float;
+    data_b_in : in float;
+    
+    data_out : out float
+  ) is
+  begin
+    data_out := data_a_in + data_b_in;
+  end ntm_scalar_adder;
+
+  procedure ntm_scalar_multiplier (
+    data_a_in : in float;
+    data_b_in : in float;
+    
+    data_out : out float
+  ) is
+  begin
+    data_out := data_a_in * data_b_in;
+  end ntm_scalar_multiplier;
+
+  procedure ntm_scalar_divider (
+    data_a_in : in float;
+    data_b_in : in float;
+    
+    data_out : out float
+  ) is
+  begin
+    data_out := data_a_in / data_b_in;
+  end ntm_scalar_divider;
+
+end ntm_scalar_arithmetic;
