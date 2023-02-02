8b36fa5c22b2a56a47324d113a8cf8b7c1286bb7
PacoReinaCampo <engineer@queenfield.tech>
2023-02-02 17:45:42 +0100

    Working in MPSoC-DV 🚀

---
+++ validation/sv/library/math/algebra/matrix/ntm_transpose_vector_product/ntm_transpose_vector_product_uvm_environment.sv
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                            __ _      _     _               //
+//                                           / _(_)    | |   | |              //
+//                __ _ _   _  ___  ___ _ __ | |_ _  ___| | __| |              //
+//               / _` | | | |/ _ \/ _ \ '_ \|  _| |/ _ \ |/ _` |              //
+//              | (_| | |_| |  __/  __/ | | | | | |  __/ | (_| |              //
+//               \__, |\__,_|\___|\___|_| |_|_| |_|\___|_|\__,_|              //
+//                  | |                                                       //
+//                  |_|                                                       //
+//                                                                            //
+//                                                                            //
+//              Peripheral-NTM for MPSoC                                      //
+//              Neural Turing Machine for MPSoC                               //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022-2025 by the author(s)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+// Author(s):
+//   Paco Reina Campo <pacoreinacampo@queenfield.tech>
+
+class peripheral_monitor;
+  virtual add_if vif;
+  mailbox monitor_to_scoreboard;
+  
+  function new(mailbox monitor_to_scoreboard, virtual add_if vif);
+    this.vif = vif;
+    this.monitor_to_scoreboard = monitor_to_scoreboard;
+  endfunction
+  
+  task run;
+    forever begin
+      peripheral_transaction monitor_transaction;
+      wait(!vif.rst);
+      @(posedge vif.clk);
+      monitor_transaction = new();
+      monitor_transaction.ip1 = vif.ip1;
+      monitor_transaction.ip2 = vif.ip2;
+      @(posedge vif.clk);
+      monitor_transaction.out = vif.out;
+      monitor_to_scoreboard.put(monitor_transaction);
+    end
+  endtask
+endclass
