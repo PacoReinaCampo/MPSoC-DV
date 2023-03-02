25ee0313eb2f74617a85c918e8c0d858ea0ec3a7
PacoReinaCampo <frareicam@gmail.com>
2023-03-03 00:02:51 +0100

    Working in MPSoC-DV 🚀

---
+++ doc/traditional/assets/chapter6/draw.sh
@@ -0,0 +1,3 @@
+dot -Tsvg dependences-mpsoc-fintech.dot > dependences-mpsoc-fintech.svg
+dot -Tsvg dependences-soc-fintech.dot > dependences-soc-fintech.svg
+dot -Tsvg dependences-pu-fintech.dot > dependences-pu-fintech.svg
