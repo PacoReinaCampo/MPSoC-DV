75cf91eb4976420878a3ba515e727e1d24cb5193
PacoReinaCampo <frareicam@gmail.com>
2023-04-19 11:12:07 +0200

    Working in MPSoC-DV 🚀

---
+++ verification/uvm/bus/apb4/library/core/peripheral_uvm_if.sv
@@ -0,0 +1,29 @@
+interface peripheral_uvm_if;
+
+  // Control flags
+  bit                has_checks = 1;
+  bit                has_coverage = 1;
+
+  // Actual Signals
+  logic              sig_clock;
+  logic              sig_reset;
+  logic       [15:0] sig_request;
+  logic       [15:0] sig_grant;
+  logic       [15:0] sig_addr;
+  logic        [1:0] sig_size;
+  logic              sig_read;
+  logic              sig_write;
+  logic              sig_start;
+  logic              sig_bip;
+  wire logic   [7:0] sig_data;
+  logic        [7:0] sig_data_out;
+  logic              sig_wait;
+  logic              sig_error;
+
+  logic              rw;
+
+  assign sig_data = rw ? sig_data_out : 8'bz;
+
+  // Coverage and assertions to be implemented here.
+
+endinterface : peripheral_uvm_if
---
+++ verification/uvm/bus/wb/library/slave/peripheral_uvm_slave_sequencer.sv
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// CLASS: peripheral_uvm_slave_sequencer
+//
+////////////////////////////////////////////////////////////////////////////////
+
+class peripheral_uvm_slave_sequencer extends uvm_sequencer #(peripheral_uvm_transfer);
+
+  // TLM port to peek the address phase from the slave monitor
+  uvm_blocking_peek_port #(peripheral_uvm_transfer) addr_ph_port;
+
+  // Provide implementations of virtual methods such as get_type_name and create
+  `uvm_component_utils(peripheral_uvm_slave_sequencer)
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+    addr_ph_port = new("addr_ph_port", this);
+  endfunction : new
+
+endclass : peripheral_uvm_slave_sequencer
