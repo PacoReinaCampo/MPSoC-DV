ddb33d68cf54888dfcef04de6deda687f668d600
PacoReinaCampo <frareicam@gmail.com>
2023-04-20 10:31:41 +0200

    Working in MPSoC-DV 🚀

--- doc/book/assets/chapter4/draw.sh
+++ doc/book/assets/chapter4/draw.sh
@@ -46,6 +46,6 @@ dot -Tsvg dependences-mpsoc-lifetech.dot > dependences-mpsoc-lifetech.svg
 dot -Tsvg dependences-soc-lifetech.dot > dependences-soc-lifetech.svg
 dot -Tsvg dependences-pu-lifetech.dot > dependences-pu-lifetech.svg
 
-dot -Tsvg dependences-mpsoc-lifetech.dot > dependences-mpsoc-fintech.svg
-dot -Tsvg dependences-soc-lifetech.dot > dependences-soc-fintech.svg
-dot -Tsvg dependences-pu-lifetech.dot > dependences-pu-fintech.svg
+dot -Tsvg dependences-mpsoc-fintech.dot > dependences-mpsoc-fintech.svg
+dot -Tsvg dependences-soc-fintech.dot > dependences-soc-fintech.svg
+dot -Tsvg dependences-pu-fintech.dot > dependences-pu-fintech.svg
