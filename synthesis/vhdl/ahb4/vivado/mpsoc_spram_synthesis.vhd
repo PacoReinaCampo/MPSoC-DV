34f077901b7e85018b58441a73d5ee00a31dc13c
PacoReinaCampo <frareicam@gmail.com>
2025-05-05 10:41:49 +0400

    Working in MPSoC-DV 🚀

--- synthesis/vhdl/ahb4/vivado/mpsoc_spram_synthesis.vhd
+++ synthesis/vhdl/ahb4/vivado/mpsoc_spram_synthesis.vhd
@@ -11,7 +11,7 @@
 --                                                                            --
 --              MPSoC-RISCV CPU                                               --
 --              Master Slave Interface Tesbench                               --
---              AMBA3 AHB-Lite Bus Interface                                  --
+--              AMBA4 AHB-Lite Bus Interface                                  --
 --                                                                            --
 --------------------------------------------------------------------------------
 
