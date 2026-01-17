f478fe37c048a2b94ab747391fac0a74850e4b09
PacoReinaCampo <frareicam@gmail.com>
2026-01-18 00:24:43 +0400

    Working in MPSoC-DV 🚀

---
+++ lifecycle/chisel/library/pu/peripheral_wb_test.scala
@@ -0,0 +1,36 @@
+package simple
+
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+// Test the ALU design
+class BitTester extends AnyFlatSpec with ChiselScalatestTester {
+
+  "BitTester test" should "pass" in {
+    test(new Bit) { dut =>
+
+      // This is exhaustive testing, which usually is not possible
+      for (a <- 0 to 15) {
+        for (b <- 0 to 15) {
+          for (op <- 0 to 3) {
+            val result =
+              op match {
+                case 0 => a + b
+                case 1 => a - b
+                case 2 => a | b
+                case 3 => a & b
+              }
+            val resMask = result & 0x0f
+
+            dut.io.fn.poke(op.U)
+            dut.io.a.poke(a.U)
+            dut.io.b.poke(b.U)
+            dut.clock.step(1)
+            dut.io.result.expect(resMask.U)
+          }
+        }
+      }
+    }
+  }
+}
